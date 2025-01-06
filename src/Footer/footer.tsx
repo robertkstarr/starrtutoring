dfea3fd41f2f759e175f9dbe9c5d338cb76af38a
Robert Starr <robert.k.starr@gmail.com>
2025-01-05 17:43:27 -0800

    Added footer.

---
+++ src/Footer/footer.tsx
@@ -0,0 +1,40 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+const SiteFooter = () => {
+  const year = new Date().getFullYear();
+  return (
+    <>
+      <AppBar
+        position="static"
+        sx={{
+          top: "auto",
+          bottom: 0,
+
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Toolbar>
+          <Typography
+            variant="subtitle1"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: { xs: 1, md: 0 },
+            }}
+          >
+            <Box sx={{ display: "flex", textAlign: "center" }}>
+              © Copyright {year} by Robert K. Starr
+            </Box>
+            <Box sx={{ display: "flex", textAlign: "center" }}>
+              All Rights Reserved
+            </Box>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default SiteFooter;
